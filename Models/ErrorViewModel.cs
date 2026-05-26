22b4862b81a33a1686058c2bcf5b0493f9cbd601
unknown <User@DESKTOP-2FNM94E.staff.uniten.local>
2026-05-26 10:35:49 +0800

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LibraryManagementSystemAimanSahharon.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
